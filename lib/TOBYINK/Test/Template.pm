08059005f94fbcbb340a12e593a723bb55e06398
Toby Inkster <mail@tobyinkster.co.uk>
2023-01-24 15:37:00 +0000

    init

---
+++ lib/TOBYINK/Test/Template.pm
@@ -0,0 +1,50 @@
+use 5.010001;
+use strict;
+use warnings;
+
+package TOBYINK::Test::Template;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.001000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+TOBYINK::Test::Template - Skeleton distribution for testing purposes
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+<https://github.com/tobyink/p5-tobyink-test-template/issues>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2023 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
